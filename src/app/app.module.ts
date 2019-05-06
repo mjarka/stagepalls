ca643aaa9704768344fafd457a7f30dfc7f61efc
Marcin Jarka <Marcin.Jarka@tccglobal.com>
2019-05-06 14:07:15 +0200

    Filters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,19 +9,23 @@ import { SidenavComponent } from './sidenav/sidenav.component';
 import { PanelComponent } from './panel/panel.component';
 import { TopbarComponent } from './topbar/topbar.component';
 import { LogopanelComponent } from './logopanel/logopanel.component';
+import { FiltersComponent } from './filters/filters.component';
 @NgModule({
   declarations: [
     AppComponent,
     SidenavComponent,
     PanelComponent,
     TopbarComponent,
-    LogopanelComponent
+    LogopanelComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModules,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
